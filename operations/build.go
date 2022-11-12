1a73c281ae68587044929571a341dae7360fd152
ahmednadr <ahmednader.an5@gmail.com>
2022-11-12 06:56:14 +0000

    create containers folder for the first time

--- operations/build.go
+++ operations/build.go
@@ -20,8 +20,12 @@ func GenerateUID(n int) string {
 
 // tar - xvzf <tar file name with .tar.gz extension>
 func Extract(path string, ContainerID string) {
-	os.Mkdir("./containers/"+ContainerID, 0755)
-	extract := exec.Command("tar", "-xvf", path, "-C", "./containers/"+ContainerID)
+	_, checkErr := os.Open("../containers")
+	if checkErr != nil {
+		os.Mkdir("../containers", 0755)
+	}
+	os.Mkdir("../containers/"+ContainerID, 0755)
+	extract := exec.Command("tar", "-xvf", path, "-C", "../containers/"+ContainerID)
 	err := extract.Run()
 	if err != nil {
 		panic(err)
