851fddad1c0d51705f9a9e3f3fe15e38d888ae62
ahmednadr <ahmednader.an5@gmail.com>
2022-11-12 07:00:36 +0000

    fix relative path

--- operations/build.go
+++ operations/build.go
@@ -20,12 +20,12 @@ func GenerateUID(n int) string {
 
 // tar - xvzf <tar file name with .tar.gz extension>
 func Extract(path string, ContainerID string) {
-	_, checkErr := os.Open("../containers")
+	_, checkErr := os.Open("./containers")
 	if checkErr != nil {
-		os.Mkdir("../containers", 0755)
+		os.Mkdir("./containers", 0755)
 	}
-	os.Mkdir("../containers/"+ContainerID, 0755)
-	extract := exec.Command("tar", "-xvf", path, "-C", "../containers/"+ContainerID)
+	os.Mkdir("./containers/"+ContainerID, 0755)
+	extract := exec.Command("tar", "-xvf", path, "-C", "./containers/"+ContainerID)
 	err := extract.Run()
 	if err != nil {
 		panic(err)
