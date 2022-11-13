9c2b64e288cb4e83164769e5311b957e01ac3e8b
ahmednadr <ahmednader.an5@gmail.com>
2022-11-13 09:21:33 +0000

    give path rather than go figuring it out

--- operations/build.go
+++ operations/build.go
@@ -73,7 +73,7 @@ func inContainerThread(c chan string) {
 
 	for bashCmd := range c {
 
-		cmd := exec.Command("echo", []string{"C", bashCmd}...)
+		cmd := exec.Command("/bin/bash", []string{"-C", bashCmd}...)
 
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
